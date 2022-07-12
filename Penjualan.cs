44dd059c8d68dcf4d96bf636c356f6ef8aa65db7
nadhiraaurell <101617886+nadhiraaurell@users.noreply.github.com>
2022-07-12 13:00:45 +0700

    Add files via upload

---
+++ Penjualan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectPenjualan
+{
+    public class Penjualan
+    {
+        public string note { get; set; }
+        public string Date { get; set; }
+        public string Customer { get; set; }
+        public int Total { get; set; }
+        public string type { get; set; }
+    }
+}
